cfbaf86897266bab22303acf7de38580717ea74e
Bobby Wang <bobbyquennell@gmail.com>
2021-09-24 11:57:33 +1000

    using Window.localStorage to remember user's dark mode choice

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,20 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { themeKey } from '../components/ColorModeToggle/ColorModeToggle';
+import { lightMode } from '../styles/sprinkles.css';
 
+
+const colorModeInitializer = `
+  (function () {
+    var colorMode = localStorage.getItem('${themeKey}') || ${lightMode};
+    document.documentElement.classList.add(colorMode);
+  })();`;
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script dangerouslySetInnerHTML={{__html: colorModeInitializer}}/>
+        </Head>
         <body>
           <Main />
           <NextScript />
