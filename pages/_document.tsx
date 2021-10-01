d4dcccd2a238b9e9b9d4c7228e47e5619d06efa1
Bobby Wang <bobbyquennell@gmail.com>
2021-10-01 23:35:10 +1000

    use `prefers-color-scheme` to detect color mode

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,20 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { themeKey } from '../components/ColorModeToggle/ColorModeToggle';
-import { lightMode } from '../styles/sprinkles.css';
+import { darkMode, lightMode } from '../styles/sprinkles.css';
 
 
 const colorModeInitializer = `
   (function () {
-    var colorMode = localStorage.getItem('${themeKey}') || ${lightMode};
-    document.documentElement.classList.add(colorMode);
+    var colorMode = localStorage.getItem('${themeKey}');
+    const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const userPrefersLight = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches;
+
+    if(colorMode === '${darkMode}' || colorMode !== '${lightMode}' && userPrefersDark){
+      document.documentElement.classList.add('${darkMode}');
+    }
+    if(colorMode === '${lightMode}' || colorMode !== '${darkMode}' && userPrefersLight){
+      document.documentElement.classList.add('${lightMode}');
+    }
   })();`;
 export default class MyDocument extends Document {
   render() {
