1920aebb0490948d18ccf47f2f6b2f8c7ccd3693
Bobby Wang <bobbyquennell@gmail.com>
2021-10-02 18:15:39 +1000

    add GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,17 @@ import '../styles/index.css'
 import '../styles/global.css'
 import React from 'react'
 import { ColorModeProvider } from '../components/ColorModeToggle/ColorModeToggle'
+import Script from 'next/script'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <ColorModeProvider><Component {...pageProps} /></ColorModeProvider>
+  return (
+    <>
+      <Script strategy="lazyOnload" src="https://www.googletagmanager.com/gtag/js?id=G-FD21L2DY4Y"></Script>
+      <Script>
+        {`window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-FD21L2DY4Y');`}
+      </Script>
+      <ColorModeProvider><Component {...pageProps} /></ColorModeProvider>
+    </>
+  )
+  
 }
